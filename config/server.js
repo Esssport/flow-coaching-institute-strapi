19294cb032b48129d1c88a40ba8a5e5512baa845
Alireza Izadjoo <aizadjoo@jirnexu.com>
2021-03-18 16:14:54 -0400

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '40756b5ab86d818c3933403f1585dad8'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd0053412-7985-4945-b3b3-a408f5b472eb'
+};
